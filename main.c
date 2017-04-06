de5a63fb6dc6ae75c83bfd87f5976e292e8804ec
TheDoctor0 <dawid.janik95@gmail.com>
2017-04-06 11:11:45 +0200

    Added more components

--- main.c
+++ main.c
@@ -5,9 +5,29 @@
 
 #define BUFFER_SIZE 1024
 
-enum { CPU, RAM, DISK, GPU, MOTHERBOARD };
+enum parts { CPU, GPU, RAM, DISK, MOTHERBOARD, SYSTEM, BIOS };
 
-char computer[MOTHERBOARD][BUFFER_SIZE];
+//"sudo lshw -short | grep system", "r");
+//"sudo cat /proc/cpuinfo | grep -E 'vendor|family|model|stepping|MHz|cache|flags'", "r");
+//"sudo dmidecode -t baseboard | grep -E 'Manufacturer|Product|Version|Serial'", "r");
+//"sudo  lshw -class volume | grep -E 'volume|description|vendor|logical|version|serial|size|capacity|configuration'", "r");
+//"sudo dmidecode -t bios | grep -E 'Vendor|Version|Date|Revision'","r");
+//"sudo dmidecode -t memory | grep -E 'Size:|Type:|Speed:|Manufacturer|Serial|Asset|Part'", "r");
+//"sudo lspci | grep -E -o '.{0,0}VGA.{0,200}'","r");
+//"sudo lspci | grep -E -o '.{0,0}Multimedia.{0,200}'","r");
+
+const char commands[][BUFFER_SIZE/4] =
+{
+    { "lscpu | grep -E 'Architecture|CPU(s)|Vendor ID|Model name|CPU MHz'" },
+    { "sudo lspci | grep -E -o '.{0,0}VGA.{0,200}'" },
+    { "sudo dmidecode -t memory | grep -E 'Size:|Type:|Speed:|Manufacturer|Serial|Part'" },
+    { "sudo lshw -class volume | grep -E 'volume|description|vendor|serial|size|capacity|configuration'" },
+    { "sudo dmidecode -t baseboard | grep -E 'Product|Manufacturer|Serial'" },
+    { "sudo lshw -c system | grep -E 'product|vendor|serial|'" },
+    { "sudo dmidecode -t bios | grep -E 'Vendor|Version|Date|Revision'" }
+};
+
+char computer[BIOS][BUFFER_SIZE];
 
 char *clean(char *str)
 {
@@ -31,7 +51,7 @@ char *clean(char *str)
     return str;
 }
 
-void execute_command(char *command, int type)
+void execute_command(const char *command, int type)
 {
     FILE *fpipe;
     char line[BUFFER_SIZE/4];
@@ -48,7 +68,7 @@ void execute_command(char *command, int type)
 	else strcat(computer[type], line);
     }
 
-    clean(computer[type]);
+    //clean(computer[type]);
 
     pclose(fpipe);
 }
@@ -63,9 +83,11 @@ int main(int argc, char **argv)
         exit(1);
     }
     
-    execute_command("sudo lshw -C memory -json | grep -e '\"id\"' -e '\"size\"' -e '\"description\"' -e '\"version\"'", CPU);
-    
-    printf(computer[CPU]);
+    for(int i = 0; i <= BIOS; i++)
+    {
+        execute_command(commands[i], i);
+        printf(computer[i]);
+    }
 
     return 0;
 }
